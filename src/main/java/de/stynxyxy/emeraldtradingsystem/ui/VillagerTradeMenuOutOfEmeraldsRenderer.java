28b914198830972a41138f5852560919026c6c4e
Jonas <131193709+DEVStynx@users.noreply.github.com>
2024-10-08 11:27:00 +0200

    Update VillagerTradeMenuOutOfEmeraldsRenderer.java

--- src/main/java/de/stynxyxy/emeraldtradingsystem/ui/VillagerTradeMenuOutOfEmeraldsRenderer.java
+++ src/main/java/de/stynxyxy/emeraldtradingsystem/ui/VillagerTradeMenuOutOfEmeraldsRenderer.java
@@ -17,7 +17,7 @@ public class VillagerTradeMenuOutOfEmeraldsRenderer {
     boolean shouldrender;
 
     public VillagerTradeMenuOutOfEmeraldsRenderer() {
-        shouldrender = true;
+        
     }
 
     @SubscribeEvent
